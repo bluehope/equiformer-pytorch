0a969f1e3cbd1d3cfdc0c14fb19518190325a54c
Hongkee Yoon <bluehope@gmail.com>
2024-02-29 19:16:18 +0900

    added PBC support

--- equiformer_pytorch/equiformer_pytorch.py
+++ equiformer_pytorch/equiformer_pytorch.py
@@ -933,6 +933,7 @@ class Equiformer(nn.Module):
         num_adj_degrees_embed = None,
         adj_dim = 0,
         max_sparse_neighbors = float('inf'),
+        use_pbc=False,                      # True: apply peridoc bound condtion (PBC)
         **kwargs
     ):
         super().__init__()
@@ -999,6 +1000,8 @@ class Equiformer(nn.Module):
 
         self.valid_radius = valid_radius
         self.num_neighbors = num_neighbors
+
+        self.use_pbc = use_pbc
 
         # main network
 
@@ -1074,7 +1077,8 @@ class Equiformer(nn.Module):
         mask = None,
         adj_mat = None,
         edges = None,
-        return_pooled = False
+        return_pooled = False,
+        lattice_vector = None # Use with PBC 
     ):
         _mask, device = mask, self.device
 
@@ -1168,6 +1172,11 @@ class Equiformer(nn.Module):
 
         indices = repeat(torch.arange(n, device = device), 'j -> b i j', b = b, i = n)
         rel_pos  = rearrange(coors, 'b n d -> b n 1 d') - rearrange(coors, 'b n d -> b 1 n d')
+        if self.use_pbc:
+            frac_rel_pos = torch.matmul(rel_pos, torch.linalg.inv(lattice_vector))
+            rel_pos_pbc  = torch.matmul(frac_rel_pos - torch.round(frac_rel_pos), lattice_vector)
+
+            rel_pos = rel_pos_pbc
 
         indices = indices.masked_select(exclude_self_mask).reshape(b, n, n - 1)
         rel_pos = rel_pos.masked_select(exclude_self_mask[..., None]).reshape(b, n, n - 1, 3)
